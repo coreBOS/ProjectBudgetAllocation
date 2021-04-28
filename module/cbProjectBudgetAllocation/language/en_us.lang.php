ccde617e3ecd3cc0b45c9508e73c320b441d708d
glorymoshi <glorysimon95@gmail.com>
2021-04-28 13:36:26 +0300

    feat(module)create module project budget allocations

---
+++ module/cbProjectBudgetAllocation/language/en_us.lang.php
@@ -0,0 +1,27 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = array(
+	'cbProjectBudgetAllocation' => 'Project-Budget Allocations',
+	'SINGLE_cbProjectBudgetAllocation' => 'Project-Budget Allocation',
+	'cbProjectBudgetAllocation ID' => 'Project-BA ID',
+
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_ProjectBudgetAllocation_INFORMATION' => 'Project-Budget Allocation',
+	'LBL_DESCRIPTION_INFORMATION' => 'Description',
+
+	'Project Budget Allocation No' => 'Project-BA No',
+	'Related To' => 'Related To',
+	'Budget Item' => 'Budget Item',
+	'Percentage' => 'Percentage',
+	'Allocated Amount' => 'Allocated Amount',
+	'Total Hours' => 'Total Hours',
+);
+?>
